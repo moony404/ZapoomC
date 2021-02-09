5e4de8d551c247b20ef83ea24a65e48e1327dde9
lefurious <69238863+lefurious@users.noreply.github.com>
2021-02-09 21:26:37 +0100

    Update ZapoomC.c

--- ZapoomC.c
+++ ZapoomC.c
@@ -1,1 +1,28 @@
-//Mettre le code ici
+#include <stdio.h>
+#include <stdlib.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+
+{
+    
+    int YN = 0;
+    
+    printf("The program is lunching ...");
+    printf("Please wait ...");
+    printf("Continue ? (1=Yes/0=No)");
+    scanf("%d", &YN);
+    
+    if (YN == 1)
+        {
+        printf("Compiling ...Done !");
+    }
+    else
+        {
+        printf("Error 203 Error, Failed loading");
+    }
+    
+    return 0;
+    
+}
