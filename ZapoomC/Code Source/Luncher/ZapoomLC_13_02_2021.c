0ea95ad1a39a56dc1e6e35f25d927cbf868778ee
LfkOff <69238863+lefurious@users.noreply.github.com>
2021-04-14 10:59:40 +0200

    Update ZapoomLC_13_02_2021.c

--- ZapoomC/Code Source/Luncher/ZapoomLC_13_02_2021.c
+++ ZapoomC/Code Source/Luncher/ZapoomLC_13_02_2021.c
@@ -1,52 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Compiler version gcc  6.3.0
+//Compiler version gcc 6.3.0 
 
 int main()
-
 {
     //---Variable---
-    
-    int YN = 0;
-    char Name[50];
-    int name;
-    FILE* fichier = NULL;
-    
+
+    int YN = 0; 
+    char Name[50]; 
+    int name; 
+
+    FILE* fichier = NULL; 
+
     //---Programme---
-    
-    printf("The program is lunching ...\n");
+
+    printf("The program is lunching ...\n"); 
+    sleep(3);
     printf("Please wait ...\n");
-    printf("Continue ? (1=Yes/0=No)\n");
+    sleep(3);
+    printf("dl : Storage/emualted/0/Zapoom\n");
+    sleep(1);
+    printf("dl : Storage/emualted/0/Zapoom/ZapoomC\n");
+    sleep(1);
+    printf("dl : Storage/emualted/0/Zapoom/ZapoomC/CodeSource\n");
+    sleep(1);
+    printf("dl : Storage/emualted/0/Zapoom/ZapoomC/CodeSource/ZapoomC\n");
+    sleep(1);
+    printf("dl : Storage/emualted/0/Zapoom/ZapoomC/CodeSource/ZapoomC/ZapoomLC_14_04_2021.c\n");
+    sleep(1);
+    printf("dl : Storage/emualted/0/Zapoom/ZapoomC/CodeSource/ZapoomC/Info.zap\n");
+    sleep(1);
+    printf("dl : Storage/emualted/0/Zapoom/ZapoomC/Setup/ZapoomC.c\n");
+    sleep(1);
+    printf("dl : Storage/emualted/0/Zapoom/ZapoomC/setup/ZapoomC.zip\n");
+    sleep(4);
+    printf("dl : Storage/emualted/0/Zapoom/ZapoomS/ZapoomS.html\n");
+    sleep(1);
+    printf("dl : Storage/emualted/0/Zapoom/setup.py\n");
+    sleep(3);
+    printf("Continue ? (1=Yes/0=No)\n"); 
     scanf("%d", &YN);
-    
+
     if (YN == 1)
-        {
-        printf("Compiling ...Done !\n");
-    }
-    else
-        {
+    {
+        printf("Compiling.\n");
+        sleep(1);
+        printf(".\n");
+        sleep(1);
+        printf("..\n");
+        sleep(1);
+        printf("...\n");
+        sleep(1);
+        printf("Done !\n");
+        sleep(3);
+    } 
+    else 
+    { 
         printf("Error 203 Error, Failed loading\n\n\n");
-    }
-    
-    //--Name & Fichier---
-    
-    printf("Whats your name ?\n");
-    scanf("%s", Name);
-    printf("Your name is %s !\n", Name);
-    
-    //---Name & Fichier---
+        exit(0);
+    } 
+
+    //--Name & Fichier--- 
+
+    printf("Whats your name ?\n"); 
+    scanf("%s", Name); 
+    printf("Your name is %s !\n", Name); 
+    sleep(1);
+
+    //---Name & Fichier--- 
     
     fichier = fopen("Info.prenom", "w");
     
-    if (fichier != NULL)
-    {
-        fprintf(fichier, "Name = %s\n", Name);
-        fclose(fichier);
-    }
-     
-    printf("\nGo to GitHub of Zapoom pls : https://github.com/lefurious/Zapoom\n");
-    
-    return 0;
+    if (fichier != NULL) 
+    { 
+        fprintf(fichier, "Name = %s\n", Name); 
+        fclose(fichier); 
+    } 
 
+    printf("\nGo to GitHub of Zapoom pls : https://github.com/lefurious/Zapoom\n\n\n\n\n\n\n\n\n\n\n"); 
+
+    return 0;
+    
 }
