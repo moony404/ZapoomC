dff1cf377ca317eab50585e45a6a4b7e82b413c7
LfkOff <69238863+lefurious@users.noreply.github.com>
2021-04-24 10:43:25 +0200

    Update ZapoomC.c

--- ZapoomC/Code Source/ZapoomC/ZapoomC.c
+++ ZapoomC/Code Source/ZapoomC/ZapoomC.c
@@ -11,17 +11,20 @@ int main()
     //---Variable---
     
     char prenom[50];
+    char pseudo_and_password[50];
+    char password[10];
     
     //---Int---
     
     int sw;
     int tutoie = 1;
+    int login_for_user = 1;
     
     //---Programme---
     
     printf("Salut toi ! :)\nJe suis un petit robot qui va\nt'aider dans ton aventure !\n");
     sleep(3);
-    printf("Quelle est ton prenom ?\n");
+    printf("Quelle est ton prenom ? (Attention, marque bien que ton prenom ! Sans espace (les tirets sont accepte))\n");
     scanf("%s", prenom);
     printf("Ton prenom est %s !\n", prenom);
     sleep(1);
@@ -36,29 +39,31 @@ int main()
     
     switch (sw)
     {
-    case 1:
-      printf("Bon Jeu !\n");
-      sleep(2);
-    break;
-    case 2:
-      printf("Fait Par Lfk2.\n");
-    break;
-    case 3:
-      printf("Ok Au-revoir :(\n");
-      exit(0);
-    break;
-    default:
-      printf("Tu n'as pas entrer un chiffre/nombre correcte donc il faut\nque tu redemarre le programme !\n");
-    break;
+    	case 1:
+      		printf("Bon Jeu !\n");
+      		sleep(2);
+    	break;
+    	case 2:
+    	  printf("Fait Par Lfk2.\n");
+    	  sleep(0);
+    	break;
+    	case 3:
+    	  printf("Ok Au-revoir :(\n");
+    	  exit(0);
+    	break;
+    	default:
+    	  printf("Tu n'as pas entrer un chiffre/nombre correcte donc il faut\nque tu redemarre le programme !\n");
+    	break;
     }
     
     printf("Hey !\n");
+    sleep(1);
     printf("Je peut te tutoier ? \n1 = oui, 2 = non.\n");
     scanf("%d", &tutoie);
     
     //---if and else---
     
-    if (tutoie = 1)
+    if (tutoie == 1)
     {
     	printf("Ok\n");
     	sleep(1);
@@ -69,9 +74,31 @@ int main()
 		sleep(1);
 	}
 	
-    printf("Veut tu te creer un compte (1),\n ou te connecter a un compte existants(2) ? \nTape le chiffre qui te corresponds et appuies sur entree !\n");
+    printf("       ---Login---\nVeut tu te creer un compte (1),\nou te connecter a un compte existants(2), ou quitter (3)? \nTape le chiffre qui te corresponds et appuies sur entree !\n");
+    scanf("%d", &login_for_user);
     
-    sleep(60);
+    switch (login_for_user)
+    {
+    	case 1:
+    		printf("Ok, mets ton pseudo et ton mot de passe coller (sans espaces entre les deux) !\n");
+    		scanf("%s", pseudo_and_password);
+    		printf("Ok\n");
+    	break;
+    	case 2:
+    		printf("Ok, mets ton mot de passe, et appuie sur entrée !\n");
+    		scanf("%s", password);
+    		printf("Tu es maintenant connecte(e) !\n");
+    		sleep(10);
+    	break;
+    	case 3:
+    		printf("Ok, :(\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    		exit(0);
+    	break;
+    	default:
+    		printf("Tu n'as pas entrer un chiffre/nombre correcte donc il faut\nque tu redemarre le programme !\n");
+    	break;
+	}
+    
     return 0;
     
 }
