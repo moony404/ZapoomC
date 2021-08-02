59a53e542fec90d8e067306fe08b84f19ba6082d
LfkOff <69238863+lefurious@users.noreply.github.com>
2021-08-02 18:37:18 +0200

    Update ZapoomC.c

--- ZapoomC/Code Source/ZapoomC/ZapoomC.c
+++ ZapoomC/Code Source/ZapoomC/ZapoomC.c
@@ -1,29 +1,49 @@
-//---Directives de préprossésseur---
+//Directives de préprossésseur
 
 #include <stdio.h>
 #include <stdlib.h>
 
-//Compiler version gcc  6.3.0
+//Compiler version TDM-GCC 4.9.2 64-bit Release
 
 int main()
 {
   
-    //---Variable---
+    //Char
     
     char user_prenom[50];
     char user_pseudoAndPassword[50];
     char user_password[10];
     
-    //---Int---
+    //Switch
+	int sw;
+    
+    //mob_touple
+    int mob_touple = 0;
+    //Info mob_touple
+    signed char mob_touple_pv = 50;
+    
+    //user
     
     int user_yon;
-    int sw;
     int user_tutoie = 1;
     int user_login = 1;
+    signed char user_firstmob = 0;
     
-    //---Programme---
+    //perso
     
-    printf("Salut toi ! :)\nJe suis un petit robot qui va\nt'aider dans ton aventure !\n");
+    int perso_nourriture = 0;
+    int perso_eau = 0;
+    int perso_force = 0;
+    int perso_lvl = 0;
+    
+    //fichier open
+    
+    FILE* fichier = NULL;
+    fichier = fopen("backup_1.txt", "w");
+    
+    //Programme
+    
+    printf("Salut toi ! :)\nAvant toute chose suis un petit robot qui va\nt'aider dans ton aventure, si il y a des bugs dans ce jeu\nn'hesites surtout pas a les dire sur ce site : https://github.com/lefurious/Zapoom/issues/new\n");
     sleep(3);
     printf("Quelle est ton prenom ? (Attention, marque bien que ton prenom ! Sans espace (les tirets sont accepte))\n");
     scanf("%s", &user_prenom);
@@ -56,13 +76,13 @@ int main()
     	  printf("Tu n'as pas entrer un chiffre/nombre correcte donc il faut\nque tu redemarre le programme !\n");
     	break;
     }
-    
+	
     printf("Hey !\n");
     sleep(1);
     printf("Je peut te tutoier ? \n1 = oui, 2 = non.\n");
     scanf("%d", &user_tutoie);
     
-    //---if and else---
+    //if and else
     
     if (user_tutoie == 1)
     {
@@ -97,54 +117,124 @@ int main()
     		exit(0);
     	break;
     	default:
+    		//Message d'erreur
     		printf("/!\ERROR/!\\nCode error : 1\nYou did not enter a correct number, you must \n restart the program!\n");
-    		sleep(7);
-    		printf("The program will close automatically in 3 seconds ...");
+    		sleep(3);
+    		printf("The program will close automatically in 3 seconds ...\n");
     		sleep(1);
-    		printf("2...");
+    		printf("2...\n");
     		sleep(1);
-    		printf("1...");
+    		printf("1...\n");
     		sleep(1);
-    		printf("0...");
+    		printf("0...\n");
     		exit(0);
     	break;
 	}
     
-    printf("Tu as faim ?\n \n1 = oui, 2 = non.\n");
+    printf("Tu as faim ?\n \n1 = oui, 0 = non.\n");
     int faim = 0;
     scanf("%d", &faim);
+    sleep(1);
     
-    //---Int de nourrituren, d'eau et de force---
-    
-    int perso_nourriture = 0;
-    int perso_eau = 0;
-    int perso_force = 0;
-    int perso_lvl = 0;
+    //Int de nourrituren, d'eau et de force
     
     if (faim == 1)
     {
     	perso_nourriture = perso_nourriture + 1;
     	perso_lvl = perso_lvl + 1;
     	printf("Tien :\n- +1 de nourriture\n- +0 d'eau\n- +0 de force\n- +1 Lvl\nTu as maintenant :\n- %d de nourriture\n- %d d'eau\n- %d de force\n- %d de level\n", perso_nourriture, perso_eau, perso_force, perso_lvl);
+    	sleep(1);
 	}
 	else
 	{
 		printf("Ok tu n'aura pas de nourriture en plus.\n");
+		sleep(1);
 	}
 	
 	printf("Tu savais que tu 1 = oui et 0 = non (dans ce jeu) ?\n");
+	sleep(2);
 	printf("As tu deja jouer a ce jeu ?\n");
 	scanf("%d", &user_yon);
 	if (user_yon == 1)
-	{
-		printf("Okay donc tu n'auras pas de tutoriel !\n");
-		sleep(1);
+	{	
+		printf("T'es sur ?\n");
+		scanf("%d", &user_yon);
+		if (user_yon == 1)
+		{
+			printf("Ok ok..\n");
+			sleep(1);
+		}
+		else
+		{
+			printf("Ok voici un court debrif sur ce que tu doit apprendre rapidement :\n- ZapoomC est un jeu de combat en console (pour l'intant).\n- Montes en niveau pour pouvoir debloquer de nouveau sort et te battre contre des monstres (appele mob).\n- Si tu as besoin d'aide rejoint notre serveur discord, lien : https://discord.gg/TsqfuZPrxa\n\n");
+			sleep(1);
+		}
 	}
 	else
 	{
 		printf("Ok voici un court debrif sur ce que tu doit apprendre rapidement :\n- ZapoomC est un jeu de combat en console (pour l'intant).\n- Montes en niveau pour pouvoir debloquer de nouveau sort et te battre contre des monstres (appele mob).\n- Si tu as besoin d'aide rejoint notre serveur discord, lien : https://discord.gg/TsqfuZPrxa\n\n");
 		sleep(1);
 	}
+	
+	
+	//Fichier pour "backup_1"
+	
+	if (fichier != NULL)
+    {
+    	//on peut écrire dans le fichier c'est bon
+    	fprintf(fichier, "Tu as maintenant :\n- %d de nourriture\n- %d d'eau\n- %d de force\n- %d de level", perso_nourriture, perso_eau, perso_force, perso_lvl);
+	}
+	else
+	{
+		//Message d'erreur
+		printf("/!\ERROR/!\\nCode error : 2\nThere was a problem opening a file, maybe for a backup!\n");
+	}
+	
+	//sleep(1);
+	sleep(1);
+	//end of //sleep(1);
+	
+	printf("AAAA (O.O)\n");
+	sleep(1);
+	printf("Un touple vient d'apparaitre !\n");
+	sleep(1);
+	printf("Que va tu faire ?\n");
+	sleep(1);
+	printf("1.L'attaquer\n2.Rebrousser chemin\n");
+	
+	/*
+	Choix de l'utilisateur
+	pour choisire ce qu'il
+	va faire donc la variable
+	créer est en haut du
+	programme (l.25, col.5)
+	*/
+	
+	scanf("%d", &user_firstmob);
+	switch (user_firstmob)
+	{
+		case 1:
+			
+		break;
+		case 2:
+			
+		break;
+		default:
+			//Message d'erreur
+    		printf("/!\ERROR/!\\nCode error : 1\nYou did not enter a correct number, you must \n restart the program!\n");
+    		sleep(3);
+    		printf("The program will close automatically in 3 seconds ...\n");
+    		sleep(1);
+    		printf("2...\n");
+    		sleep(1);
+    		printf("1...\n");
+    		sleep(1);
+    		printf("0...\n");
+    		exit(0);
+		break;
+	}
+	
+	fclose(fichier);
 	
     return 0;
     
