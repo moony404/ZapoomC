14b82511e48d8c318cf01704cca406c9f7fa36ce
LfkOff <69238863+lefurious@users.noreply.github.com>
2021-08-03 09:24:03 +0200

    Update ZapoomC.c

--- ZapoomC/Code Source/ZapoomC/ZapoomC.c
+++ ZapoomC/Code Source/ZapoomC/ZapoomC.c
@@ -27,8 +27,9 @@ int main()
     int user_yon;
     int user_tutoie = 1;
     int user_login = 1;
-    signed char user_firstmob = 0;
-    
+    signed char user_request_firstmob = 0;
+    signed char user_request_regenpv = 1;
+	    
     //perso
     
     int perso_nourriture = 0;
@@ -195,13 +196,16 @@ int main()
 	sleep(1);
 	//end of //sleep(1);
 	
+	//Premmier mob
+			
 	printf("AAAA (O.O)\n");
 	sleep(1);
 	printf("Un touple vient d'apparaitre !\n");
 	sleep(1);
 	printf("Que va tu faire ?\n");
-	sleep(1);
+	printf("<><><><><>Menu<><><><><>\n\n");
 	printf("1.L'attaquer\n2.Rebrousser chemin\n");
+	sleep(1);
 	
 	/*
 	Choix de l'utilisateur
@@ -211,16 +215,157 @@ int main()
 	programme (l.25, col.5)
 	*/
 	
-	scanf("%d", &user_firstmob);
-	switch (user_firstmob)
+	scanf("%d", &user_request_firstmob);
+	switch (user_request_firstmob)
 	{
 		case 1:
 			//Attaque
 			sleep(1);
 			printf("Ok, mais.. il semblerait que tu n'a pas d'arme !\nTu pert donc 5 pv, car le touple t'as attaque !\n");
-			perso_pv = perso_pv - 5;
-			printf("Tu as maintenant %d de vie !\n", perso_pv);
 			sleep(5);
+			perso_pv = perso_pv - 5;
+			printf("Tu as maintenant %d points de vie !\n", perso_pv);
+			sleep(3);
+			
+			/*
+			Début du système pour la regen qui a 137 ligne en comptant les comments...
+			
+			Check si perso_ a bien
+			tout ses pv dans perso_pv, 
+			pour sa on utilise un 
+			simple "if else"
+			Pour se faire voici le code :
+			*/
+			
+			if (perso_pv < 50)
+			{
+				printf("A zut ... tu n'as plus que %d point de vie !\nA moins que la nourriture te regenere de la vie !\nVeux-tu prendre un peu de ta nourriture pour pouvoir\nte regenerer ?\n", perso_pv);
+				scanf("%d", &user_yon);
+				if (user_yon == 1)
+				{
+					if (perso_nourriture == 0)
+					{
+						//sa match pas
+						printf("Malheuresement tu ne peux pas te regenerer car tu n'as pas ou plus de nourriture,\ndonc tu ne regenereras pas :(\n");
+					}
+					else
+					{
+						//tout match (nourriture + pv) alors sa fait le truc de switch (user_request_regenpv)
+						//le switch est déplacé ici !
+						printf("Ok, combien de point de vie souhaites tu regenerer ?\n(Tu peux en regenerer que 10 a la fois), (Sachant que tu en a %d.)\n", perso_pv);
+						//Deamnde des pv, 
+						//donc creation 
+						//d'une nouvelle 
+						//variable pour le 
+						//choix en signed 
+						//char (l.31, col.5)
+						scanf("%d", &user_request_regenpv);
+					
+						//Switch avec la variable user_request_regenpv
+					
+						switch (user_request_regenpv)
+						{
+							case 1:
+								printf("Ok tu as pris 1 pv -> (point(s) de vie) en plus.\n");
+								perso_pv = perso_pv + 1;
+								printf("Tu en as maintenant %d\n", perso_pv);
+							break;
+							case 2:
+								printf("Ok tu as pris 2 pv -> (point(s) de vie) en plus.\n");
+								perso_pv = perso_pv + 2;
+								printf("Tu en as maintenant %d\n", perso_pv);
+							break;
+							case 3:
+								printf("Ok tu as pris 3 pv -> (point(s) de vie) en plus.\n");
+								perso_pv = perso_pv + 3;
+								printf("Tu en as maintenant %d\n", perso_pv);
+							break;
+							case 4:
+								printf("Ok tu as pris 4 pv -> (point(s) de vie) en plus.\n");
+								perso_pv = perso_pv + 4;
+								printf("Tu en as maintenant %d\n", perso_pv);
+							break;
+							case 5:
+								printf("Ok tu as pris 5 pv -> (point(s) de vie) en plus.\n");
+								perso_pv = perso_pv + 5;
+								printf("Tu en as maintenant %d\n", perso_pv);
+							break;
+							case 6:
+								printf("Ok tu as pris 6 pv -> (point(s) de vie) en plus.\n");
+								perso_pv = perso_pv + 6;
+								printf("Tu en as maintenant %d\n", perso_pv);
+							break;
+							case 7:
+								printf("Ok tu as pris 7 pv -> (point(s) de vie) en plus.\n");
+								perso_pv = perso_pv + 7;
+								printf("Tu en as maintenant %d\n", perso_pv);
+							break;
+							case 8:
+								printf("Ok tu as pris 8 pv -> (point(s) de vie) en plus.\n");
+								perso_pv = perso_pv + 8;
+								printf("Tu en as maintenant %d\n", perso_pv);
+							break;
+							case 9:
+								printf("Ok tu as pris 9 pv -> (point(s) de vie) en plus.\n");
+								perso_pv = perso_pv + 9;
+								printf("Tu en as maintenant %d\n", perso_pv);
+							break;
+							case 10:
+								printf("Ok tu as pris 10 pv -> (point(s) de vie) en plus.\n");
+								perso_pv = perso_pv + 10;
+								printf("Tu en as maintenant %d\n", perso_pv);
+							break;
+							default:
+								//Message d'erreur
+    							printf("/!\ERROR/!\\nCode error : 1\nYou did not enter a correct number, you must \n restart the program!\n");
+    							sleep(3);
+    							printf("The program will close automatically in 3 seconds ...\n");
+    							sleep(1);
+    							printf("2...\n");
+    							sleep(1);
+    							printf("1...\n");
+    							sleep(1);
+	    						printf("0...\n");
+	    						exit(0);
+							break;
+						}
+					}
+				}
+				else
+				{
+					printf("Ok tu ne regeneras pas, attends la fin d'un autre combat pour regenerer !\n");
+					//puis, plus rien, j'ai deplacer le suite en Ctrl + X a la ligne 252, colonne 25.
+				}
+			}
+			else
+			{
+				//Rien ne se passe,
+				//Aucune erreur,
+				//On peut continuer,
+				//Sans aucune frayeurs.
+				//
+				//Lfk2,
+				//03/08/2021
+			}
+			
+			/*
+			Et voila, tout simplement 
+			comme ceci en testant la 
+			valeur de perso_pv, si 
+			elle est < (infétieur) 
+			à 1, si oui on stop le 
+			jeu en disans qu'on a 
+			perdu et on rééxplique 
+			que pour relancer un 
+			partie il faut faire 
+			Alt + F4 ou la croix 
+			pour fermer un programme, 
+			puis le démarrer.
+			Apres on demande si
+			l'utilisateur veut regen 
+			ses pv avec sa perso_nourriture
+			*/
+			
 			printf("Le touple s'en va sans degat avec %d de vie !\n", mob_touple_pv);
 			sleep(1);
 			printf("Veux-tu en savoir plus sur cette bete ferosse ?\n");
@@ -234,7 +379,12 @@ int main()
 				sleep(1);
 				printf(".....\n");
 				sleep(1);
-				printf("Le touple est une creature féroce qui va a la rencontre des joueurs pour les affronter, au plus pronfond de lui se cache des sentiments de regret... ce mot ne devrais meme pas existé !, mais bon c'est comme les ratata, c'est le monstre basic !, a present quelques caractéristiques sur le touple :\n- Grande vie avec 50 points de vie (pv)\n- Type : ocean\n- Sexe : male\n\nVoila, maintenant tu t'y connais un peu plus sur le touple !\n");
+				printf("Le touple est une creature féroce qui va a la rencontre des joueurs pour les affronter,\nau plus pronfond de lui se cache des sentiments de regret... ce mot ne devrais meme pas existe !\nmais bon c'est comme les ratata, c'est le monstre basic !\nA present quelques caracteristiques sur le touple :\n\n- Grande vie avec 50 points de vie (pv)\n- Type : ocean\n- Sexe : male\n\nVoila, maintenant tu t'y connais un peu plus sur le touple !\n");
+			}
+			else
+			{
+				printf("Ok, dommage tu pourrais en apprendre plus !\n");
+				sleep(1);
 			}
 		break;
 		case 2:
