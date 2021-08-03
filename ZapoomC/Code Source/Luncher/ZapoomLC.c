e297e07e0e31a656062c73168b3702df0d9dc15f
LfkOff <69238863+lefurious@users.noreply.github.com>
2021-08-03 07:42:38 +0200

    Update and rename ZapoomLC_13_02_2021.c to ZapoomLC.c

---
+++ ZapoomC/Code Source/Luncher/ZapoomLC.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Compiler version gcc 6.3.0 
+
+int main()
+{
+    //---Programme---
+    printf("The program is lunching ...\n"); 
+    sleep(1);
+    printf("Please wait few second...\n");
+    sleep(2);
+    printf("Downloading : Makefile.win\n");
+    sleep(1);
+    printf("Downloading : ZapoomC.o\n");
+    sleep(5);
+    printf("Downloading : ZapoomC.c\n");
+    sleep(1);
+    printf("Downloading : ZapoomC.exe\n");
+    sleep(3);
+    printf("Downloading : README.txt\n");
+    sleep(3);
+	printf("Done !\n");
+	sleep(1);
+
+    printf("\nGo to GitHub of Zapoom pls : https://github.com/lefurious/Zapoom\n");
+    sleep(60);
+    exit(0);
+
+    return 0;
+    
+}
