9b5e4ab8ba7da53714d0f6ee159458cc4125b41f
LfkOff <69238863+lefurious@users.noreply.github.com>
2021-08-07 10:05:46 +0200

    Update ZapoomC.c

--- ZapoomC/Code Source/ZapoomC/ZapoomC.c
+++ ZapoomC/Code Source/ZapoomC/ZapoomC.c
@@ -55,6 +55,7 @@ int main()
     //perso
     
     int perso_inv_bois = 0;
+    int perso_inv_planches = 0;
     int perso_inv_caillou = 0;
     int perso_inv_fil = 0;
     int perso_inv_baton = 0;
@@ -76,7 +77,7 @@ int main()
     
     printf("Salut toi ! :)\nAvant toute chose suis un petit robot qui va\nt'aider dans ton aventure, si il y a des bugs dans ce jeu\nn'hesites surtout pas a les dire sur ce site : https://github.com/lefurious/Zapoom/issues/new\n\n");
     sleep(8);
-    printf("Desoloer si les accents ne s'affiche pas,\nceci est lie a un bug, il faudra vous y faire, quelque fois il y a ER pour demarquer les accents sinon des fois\non peut comprendre d'autre mots. Merci de votre comphreensions.\n\n");
+    printf("Desoler si les accents ne s'affiche pas,\nceci est lie a un bug, il faudra vous y faire, quelque fois il y a ER pour demarquer les accents sinon des fois\non peut comprendre d'autre mots. Merci de votre comphreensions.\n\n");
     sleep(10);
     printf("Quelle est ton prenom ? (Attention, marque bien que ton prenom ! Sans espace (les tirets sont accepte))\n");
     scanf("%s", &user_prenom);
@@ -193,7 +194,7 @@ int main()
     printf("Hey !\n");
     sleep(1);
     printf("Je peut te tutoier ? \n1 = oui, 2 = non.\n");
-    scanf("%d", &user_tutoie);
+    scanf("%d\n", &user_tutoie);
     
     //if and else
     
@@ -207,7 +208,7 @@ int main()
 		printf("Je TE tutoie quand meme !\n\n\n");
 		sleep(1);
 	}
-	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	printf("\n\n\n\n\n\n\n\n\n\n\n\\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nn\n\n\n\n\n\n\n\n\n\n\n\n");
     printf("       ---Login---\nVeut tu te creer un compte (1),\nou te connecter a un compte existants(2), ou quitter (3)? \nTape le chiffre qui te corresponds et appuies sur entree !\n");
     scanf("%d", &user_login);
     
@@ -225,7 +226,7 @@ int main()
     		sleep(2);
     	break;
     	case 3:
-    		printf("Ok, :(\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    		printf("Ok, :(\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     		sleep(1);
     		exit(0);
     	break;
@@ -462,52 +463,52 @@ int main()
 						switch (user_request_regenpv)
 						{
 							case 1:
-								printf("Ok tu as pris 1 pv -> (point(s) de vie) en plus.\n");
+								printf("Ok tu as pris 1 pv -> (point de vie) en plus.\n");
 								perso_pv = perso_pv + 1;
 								printf("Tu en as maintenant %d\n", perso_pv);
 							break;
 							case 2:
-								printf("Ok tu as pris 2 pv -> (point(s) de vie) en plus.\n");
+								printf("Ok tu as pris 2 pv -> (points de vie) en plus.\n");
 								perso_pv = perso_pv + 2;
 								printf("Tu en as maintenant %d\n", perso_pv);
 							break;
 							case 3:
-								printf("Ok tu as pris 3 pv -> (point(s) de vie) en plus.\n");
+								printf("Ok tu as pris 3 pv -> (points de vie) en plus.\n");
 								perso_pv = perso_pv + 3;
 								printf("Tu en as maintenant %d\n", perso_pv);
 							break;
 							case 4:
-								printf("Ok tu as pris 4 pv -> (point(s) de vie) en plus.\n");
+								printf("Ok tu as pris 4 pv -> (points de vie) en plus.\n");
 								perso_pv = perso_pv + 4;
 								printf("Tu en as maintenant %d\n", perso_pv);
 							break;
 							case 5:
-								printf("Ok tu as pris 5 pv -> (point(s) de vie) en plus.\n");
+								printf("Ok tu as pris 5 pv -> (points de vie) en plus.\n");
 								perso_pv = perso_pv + 5;
 								printf("Tu en as maintenant %d\n", perso_pv);
 							break;
 							case 6:
-								printf("Ok tu as pris 6 pv -> (point(s) de vie) en plus.\n");
+								printf("Ok tu as pris 6 pv -> (points de vie) en plus.\n");
 								perso_pv = perso_pv + 6;
 								printf("Tu en as maintenant %d\n", perso_pv);
 							break;
 							case 7:
-								printf("Ok tu as pris 7 pv -> (point(s) de vie) en plus.\n");
+								printf("Ok tu as pris 7 pv -> (points de vie) en plus.\n");
 								 perso_pv = perso_pv + 7;
 								printf("Tu en as maintenant %d\n", perso_pv);
 							break;
 							case 8:
-								printf("Ok tu as pris 8 pv -> (point(s) de vie) en plus.\n");
+								printf("Ok tu as pris 8 pv -> (points de vie) en plus.\n");
 								perso_pv = perso_pv + 8;
 								printf("Tu en as maintenant %d\n", perso_pv);
 							break;
 							case 9:
-								printf("Ok tu as pris 9 pv -> (point(s) de vie) en plus.\n");
+								printf("Ok tu as pris 9 pv -> (points de vie) en plus.\n");
 								perso_pv = perso_pv + 9;
 								printf("Tu en as maintenant %d\n", perso_pv);
 							break;
 							case 10:
-								printf("Ok tu as pris 10 pv -> (point(s) de vie) en plus.\n");
+								printf("Ok tu as pris 10 pv -> (points de vie) en plus.\n");
 								perso_pv = perso_pv + 10;
 								printf("Tu en as maintenant %d\n", perso_pv);
 							break;
@@ -607,7 +608,7 @@ int main()
 				else
 				{
 					printf("Ok tu ne regeneras pas, attends la fin d'un autre combat pour regenerer !\n");
-					//puis, plus rien, j'ai deplacer les infos utiles au debut avec le raccourcis Ctrl + X a la ligne 252, colonne 25.
+					//puis, plus rien
 				}
 			}
 			else
@@ -753,17 +754,17 @@ int main()
 	sleep(3); //la on sort avec un sleep	
 	
 	/*
-	Suite des choses, 
-	donc là on passé 
-	le cap des 700
-	lignes mais la 
-	on va faire le 
-	système de récolte 
-	de bois, pour 
-	pouvoir récolter 
-	du bois pour 
-	pouvoir crafter 
-	des outils, 
+	Suite des choses,
+	donc là on passé
+	le cap des 750
+	lignes mais la
+	on va faire le
+	système de récolte
+	de bois, pour
+	pouvoir récolter
+	du bois pour
+	pouvoir crafter
+	des outils,
 	arme, etc...
 	*/
 	
@@ -829,7 +830,140 @@ int main()
 	
 	//sleep de sortie des syst
 	sleep(2);
+	//suite du programme pour avprès le bois :
+	printf("Tu vient d'avoir ta premiere buche de bois !\ndonc tu peut dorenavant fabriquer des objets avec ce bois !");
+	printf("<><><><><>Menu De Fabriquation Simple<><><><><>\n\n");
+	printf("Que veux-tu fabriquer ?\n\n");
+	printf("1.Planches (x8)\n2.Baton (x4)\n\n");
+	scanf("%d", &sw);
+	switch (sw)
+	{
+		case 1:
+			printf("Ok, attends un peu !\n");
+			sleep(2);
+    		printf("0  - [..........]\n");
+    		sleep(1);
+    		printf("48 - [||||......]\n");
+    		sleep(1);
+    		printf("64 - [||||||....]\n");
+    		sleep(1);
+    		printf("73 - [|||||||...]\n");
+    		sleep(1);
+    		printf("100 -[||||||||||]\n");
+    		sleep(1);
+    		printf("Terminee !\n");
+    		perso_inv_planches = perso_inv_planches + 8;
+    		sleep(1);
+    		printf("Te voila avec des planches !");
+    		sleep(1);
+		break;
+		case 2:
+			printf("Ok, attends un peu !\n");
+			sleep(2);
+    		printf("0  - [..........]\n");
+    		sleep(1);
+    		printf("48 - [||||......]\n");
+    		sleep(1);
+    		printf("64 - [||||||....]\n");
+    		sleep(1);
+    		printf("73 - [|||||||...]\n");
+    		sleep(1);
+    		printf("100 -[||||||||||]\n");
+    		sleep(1);
+    		printf("Terminee !\n");
+    		perso_inv_baton = perso_inv_baton + 4;
+    		sleep(1);
+    		printf("Te voila avec des batons !");
+    		printf("Bravo : succes deverouille :\nCFabriquer son premiere objet.\n\n");
+    		sleep(3);
+		break;
+		default:
+			/*
+			Début du
+			mesage d'erreur !
+			*/
+    		printf("/!\ERROR/!\\nCode error : 1\nYou did not enter a correct number, you must \n restart the program!\n");
+    		sleep(3);
+    		printf("Fermeture des fichiers ouvert ?\n");
+    		sleep(1);
+    		if (fichier != NULL)
+    		{
+    			printf("Tentative de redemarrage des fichiers...\n");
+    			sleep(2);
+    			printf("0  - [..........]\n");
+    			sleep(1);
+    			printf("12 - [|.........]\n");
+    			sleep(1);
+    			printf("24 - [||........]\n");
+    			sleep(1);
+    			printf("30 - [|||.......]\n");
+    			sleep(1);
+    			printf("46 - [||||......]\n");
+    			sleep(1);
+    			printf("51 - [|||||.....]\n");
+    			sleep(1);
+    			printf("68 - [||||||....]\n");
+    			sleep(1);
+    			printf("68 - [||||||....]\n");
+    			sleep(1);
+    			printf("70 - [|||||||...]\n");
+    			sleep(1);
+    			printf("82 - [||||||||..]\n");
+    			sleep(1);
+    			printf("89 - [||||||||..]\n");
+    			sleep(1);
+    			printf("90 - [|||||||||.]\n");
+    			sleep(1);
+    			printf("95 - [|||||||||.]\n");
+    			sleep(1);
+    			printf("96 - [|||||||||.]\n");
+    			sleep(1);
+    			printf("98 - [|||||||||.]\n");
+    			sleep(1);
+    			printf("99 - [|||||||||.]\n");
+    			sleep(1);
+    			printf("99 - [|||||||||.]\n");
+    			sleep(1);
+    			printf("100 -[||||||||||]\n");
+    			sleep(1);
+    			printf("Terminee !\n");
+    			//debut de la 2eme verif de fichier != NULL
+    			if (fichier != NULL)
+    			{
+    				if (user_tutoie == 1) //la variable tutoie sert une fois au moins ;=)
+    				{
+    					printf("Recuperation termine ! Tu as eu de la chance !\n");
+					}
+    				else
+    				{
+    					printf("Recuperation termine ! Vous avez eu de la chance !\n");
+					}
+				}
+			}
+			else
+			{
+				printf("Les fichiers ne peuvent pas etre fermer donc il faut que tu \nredemarre le programme mais ne t'inquite pas il va redemarrer tout seul !\n");
+			}
+    		printf("OK !\n");
+    		sleep(1);
+    		printf("The program will close automatically in 3 seconds ...\n");
+    		sleep(1);
+    		printf("2...\n");
+    		sleep(1);
+    		printf("1...\n");
+    		sleep(1);
+    		printf("0...\n");
+    		exit(1);
+    		
+    		/*
+			Fin du
+			message d'erreur !
+			*/
+		break;
+	}
 	
+	//switch (avec sw)
+		
 	//juste avoir de l'oxigene
 	printf("\n\n");
 	
@@ -839,7 +973,7 @@ int main()
 	
 	//recalcule pour "backup_1.txt" :
 	//Fichier pour "backup_1.txt"
-	
+	fichier = fopen("backup_2.txt", "w");
 	if (fichier != NULL)
     {
     	//on peut écrire dans le fichier c'est bon
@@ -945,10 +1079,10 @@ int main()
     //user_score = :
     user_score = perso_inv_caillou + perso_inv_bois + perso_inv_fil + perso_inv_baton + perso_inv_carteunoreverse + perso_inv_laine + perso_inv_toupli + perso_nourriture + perso_eau + perso_force + perso_lvl + perso_pv;
     //suite
-    printf("%d", user_score); // a enlever si sa marche
 	//fin, snif...
 	printf("Ton score : %d, tu peut etre fier de toi jeune aventurier !\n", user_score);
 	printf("_____________________________________________________________________\n"); //ligne d'espace
+	printf("A la fin tu peut voir ton temps en seconde,\n il est marqué juste après le 'process exited after : <est la ton temps est marqué>'\n");
 	
 	fclose(fichier); //fermeture des fichiers
 	
